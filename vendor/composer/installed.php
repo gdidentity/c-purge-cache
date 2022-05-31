43745acfe0f789bfc181a509fbd84b5db6c1ac17
Jakub Bebjak <bebjakub@gmail.com>
2022-05-31 12:44:04 +0200

    add vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '2f1e9f213c731d8da439164110736e927f95ae2e',
+        'name' => 'gdidentity/c-purge-cache',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gdidentity/c-purge-cache' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '2f1e9f213c731d8da439164110736e927f95ae2e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
